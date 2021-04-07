fa604e2176983a66bfe47e9e5e4710c300ffc90f
RobbanGit <81112288+RobbanGit@users.noreply.github.com>
2021-04-07 10:55:26 +0200

    Basic Obstacle Avoidance

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,37 @@
 #include <Smartcar.h>
 
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+const int TRIGGER_PIN = 6;
+const int ECHO_PIN = 7;
+const unsigned int MAX_DISTANCE = 300;
+const int minimumDistance = 50;
+
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+SimpleCar car(control);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+    car.setSpeed(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(front.getDistance());
+  delay(10);
+
+  if (front.getDistance() <= minimumDistance && front.getDistance() > 0)
+  {
+    car.setSpeed(0);
+  }
+  else if(front.getDistance() > minimumDistance) {
+    car.setSpeed(5);
+    Serial.println("Object Ahoy!");
+  }
+  else{
+    Serial.println("No objects in sight!");
+  }
+  
 }
