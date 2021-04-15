84ea5947377db5d3cfb07af94d3e29c19c7adef9
asiyamohammed <gusasiymo@student.gu.se>
2021-04-15 16:08:59 +0200

    distance travelled and timer
    Co-authored-by:Johan Axell<gusaxelljo@student.gu.se>

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,10 +1,14 @@
 #include <MQTT.h>
+#include <WiFi.h>
 #ifdef __SMCE__
 #include <OV767X.h>
 #endif
 
 #include <Smartcar.h>
 
+#ifndef __SMCE__
+WiFiClient net;
+#endif
 MQTTClient mqtt;
 
 ArduinoRuntime arduinoRuntime;
@@ -14,7 +18,6 @@ DifferentialControl control(leftMotor, rightMotor);
 
 SimpleCar car(control);
 
-
 const auto oneSecond = 1000UL;
 const auto triggerPin = 6;
 const auto echoPin = 7;
@@ -22,46 +25,67 @@ const auto maxDistance = 400;
 const auto minimumDistance = 50;
 SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
 
+const auto pulsesPerMeter = 600;
 
+DirectionlessOdometer leftOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::leftOdometerPin,
+    []() { leftOdometer.update(); },
+    pulsesPerMeter);
+DirectionlessOdometer rightOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::rightOdometerPin,
+    []() { rightOdometer.update(); },
+    pulsesPerMeter);
+
+DistanceCar car2(arduinoRuntime, control, leftOdometer, rightOdometer);
 int speed = 100;
 
 
+int hardTurn = 30;
+
 auto message_func = [](String topic, String message){
       if (message == "forward") {
-        car.setSpeed(speed);
+        car.setSpeed(100);
       } else if (message == "backward") {
-        car.setSpeed(speed*-1);
+          car.setSpeed(-100);
+      } else if (message == "right") {
+          car.setAngle(30);
+      } else if (message == "left") {
+          car.setAngle(-30);
+      } else if (message == "straight") {
+          car.setAngle(0);
+      } else if (message == "stop") {
+          car.setSpeed(0);
       } else {
         Serial.println(topic + " " + message);
       }
 };
 
+
+    
+
 void setup() {
   Serial.begin(9600);
-  
+  car2.enableCruiseControl();
+  mqtt.begin(WiFi);
 
   if(mqtt.connect("username", "username", "password" ))
   {
-    mqtt.subscribe("SMCE-Group11", 1);
+    mqtt.subscribe("topic/#", 1);
     mqtt.onMessage(message_func);
   }
 }
 
 void loop() {
-if (mqtt.connected()) 
+  car2.update();
+  if (mqtt.connected()) 
   {
-      mqtt.loop();
-  }
-  int currDist = front.getDistance();
-  Serial.println("Front dist: " + currDist);
-  delay(10);
-
-  if (currDist <= minimumDistance && currDist > 0)
-  {
-    car.setSpeed(0);
-  }
-  else if(currDist > minimumDistance) {
-    car.setSpeed(5);
-    Serial.println("Object Ahoy!");
-  }
+      mqtt.loop(); 
+     }
+   if(car2.getSpeed()!= 0){
+   const auto travelledDistance =String(car2.getDistance());
+   mqtt.publish("topic/distance", travelledDistance);
+   }
+   delay(500);
 }
